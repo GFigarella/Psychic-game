fdd8b616109b6f1c9578ac122c5c100247b94fc2
Gabriel Figarella <GFigarella@Gabriels-MacBook-Pro.local>
2017-12-06 10:02:21 -0500

    added the html and javascript code, as well as organized the folder structure

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Psychic-game
+
+* This will be  game where the 'computer' will guess a letter randomly from the alphabet, and the user has to guess which letter the 'computer' is thinking of. Each guess will be stored and displayed so that the user knows what leter he/she has tried so far. 
+
+* The user will have a maximum number of guesses.
+
+* If the user runs out of guesses, the 'Losses' count goes up by 1.
+* If the user guesses the letter correctly, the 'Wins' count go up by 1.
---
+++ assets/javascript/game.js
@@ -0,0 +1,57 @@
+var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var myGuesses = [];
+console.log(myGuesses);
+
+// Set all variables to their starting state, 0 wins, 0 losses and 8 guesses left
+var wins = 0;
+var losses = 0;
+var guessesLeft = 8;
+
+// Randomly pick a letter form the letters array
+var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+console.log("computer guess is " + computerGuess);
+
+// A reset function that sets everything to its default state
+function reset(){
+    // reset # of guesses left
+    guessesLeft = 8;
+    document.querySelector("#guessLeft").innerHTML = guessesLeft;
+    // resets myGuesses array
+    myGuesses = [];
+    document.querySelector("#myGuess").innerHTML = myGuesses;
+    // resets the computer guess
+    var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+    console.log("new computer guess is " + computerGuess);
+    var userGuess;
+    return;
+}
+
+// Main event that happens when a key is pressed by the user
+document.onkeyup = function(event){
+    //storing user guess in a variable
+    var userGuess = event.key.toLowerCase();
+
+    if (userGuess === computerGuess){
+        // increment wins by 1, assign the new wins variable to the html element with the wins ID
+        wins++; 
+        document.querySelector("#wins").innerHTML = wins;
+        console.log("my guess is " + userGuess);
+        reset();
+    }
+
+    else{
+        // Decrement guesses left, assign that new value to the html element with the guessLeft ID, and push the guesses made by the user to the myGuesses array
+        if (guessesLeft > 1){
+            guessesLeft--;
+            document.querySelector("#guessLeft").innerHTML = guessesLeft;
+            myGuesses.push(userGuess);
+            document.querySelector("#myGuess").innerHTML = myGuesses;
+        }
+
+        else{
+            losses++;
+            document.querySelector("#losses").innerHTML = losses;
+            reset();
+        }
+    }
+}
